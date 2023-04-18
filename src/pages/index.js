f77e5161ca3ad73bdeaa41a48ba185d42541d142
Robin Bergstrand <robin.bergstrand@chasacademy.se>
2023-04-18 02:30:13 +0200

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,124 +1,220 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    return (
+        <div className="max-w-7xl px-24 mx-auto ">
+            <header className="border-b-2 border-gray-300">
+                <nav>
+                    <ul className="flex h-20 items-center text-lg font-semibold">
+                        <li>
+                            <Link href={"/"}>Home</Link>
+                        </li>
+                        <li className="ml-auto mr-16">
+                            <Link href={"/"}>Projects</Link>
+                        </li>
+                        <li>
+                            <button className="bg-blue-700 py-2 px-4 rounded-lg text-white">
+                                Contact
+                            </button>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            <main className="flex min-h-screen flex-col py-24">
+                <section className="flex mt-10">
+                    <div className="flex flex-col w-7/12 items-start gap-2 mt-20">
+                        <h2>Web Developer</h2>
+                        <h1 className="text-5xl mb-4">Robin Bergstrand</h1>
+                        <p className="mb-4">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua. Nam libero justo laoreet sit
+                            amet cursus sit. Lorem ipsum dolor sit amet,
+                            consectetur adipiscing elit, sed do eiusmod tempor
+                            incididunt ut labore et dolore magna aliqua. Nam
+                            libero justo laoreet sit amet cursus sit.
+                        </p>
+                        <button className="bg-blue-700 py-2 px-4 rounded-lg text-white">
+                            Contact me
+                        </button>
+                    </div>
+                    <div>
+                        <Image width="500" height="300" src="/hero.svg"></Image>
+                    </div>
+                </section>
+                <section className="mt-40">
+                    <h2 className="text-4xl mb-10">Skills</h2>
+                    <div className="flex gap-10 justify-between font-semibold">
+                        <div>
+                            <Image
+                                width="100"
+                                height="100"
+                                src={"/js.bmp"}
+                                className="h-[100px]"
+                            ></Image>
+                            <h3 className="text-center mt-2">JavaScript</h3>
+                        </div>
+                        <div>
+                            <Image
+                                width="100"
+                                height="100"
+                                src={"/react.svg"}
+                                className="h-[100px]"
+                            ></Image>
+                            <h3 className="text-center mt-2">React</h3>
+                        </div>
+                        <div>
+                            <Image
+                                width="100"
+                                height="100"
+                                src={"/tailwind.svg"}
+                                className="h-[100px]"
+                            ></Image>
+                            <h3 className="text-center mt-2">Tailwind</h3>
+                        </div>
+                        <div>
+                            <Image
+                                width="100"
+                                height="100"
+                                src={"/git.svg"}
+                                className="h-[100px]"
+                            ></Image>
+                            <h3 className="text-center mt-2">Git</h3>
+                        </div>
+                        <div>
+                            <Image
+                                width="100"
+                                height="100"
+                                src={"/redux.svg"}
+                                className="h-[100px]"
+                            ></Image>
+                            <h3 className="text-center mt-2">Redux</h3>
+                        </div>
+                        <div>
+                            <Image
+                                width="100"
+                                height="100"
+                                src={"/mysql.svg"}
+                                className="h-[100px]"
+                            ></Image>
+                            <h3 className="text-center mt-2">MySQL</h3>
+                        </div>
+                        <div>
+                            <Image
+                                width="100"
+                                height="100"
+                                src={"/html.svg"}
+                                className="h-[100px]"
+                            ></Image>
+                            <h3 className="text-center mt-2">HTML</h3>
+                        </div>
+                        <div>
+                            <Image
+                                width="72"
+                                height="72"
+                                src={"/css.svg"}
+                                className="h-[100px]"
+                            ></Image>
+                            <h3 className="text-center mt-2">CSS</h3>
+                        </div>
+                    </div>
+                </section>
+                <section className="flex flex-col">
+                    <h2 className="text-4xl mb-10 mt-36">Projects</h2>
+                    <div className="flex gap-8">
+                        <div className="w-1/2 rounded-md overflow-hidden drop-shadow-2xl flex flex-col">
+                            <Image
+                                className="h-96 object-cover object-center"
+                                src={"/wordle.png"}
+                                width="1000"
+                                height="1000"
+                            ></Image>
+                            <div className="p-6 border-2 flex-grow flex flex-col">
+                                <h3 className="text-2xl mb-2">WORDLE</h3>
+                                <div className="flex gap-2 mb-4">
+                                    <h4 className="px-2 py-1 bg-blue-200 rounded-md text-sm">
+                                        JavaScript
+                                    </h4>
+                                    <h4 className="px-2 py-1 bg-blue-200 rounded-md text-sm">
+                                        React
+                                    </h4>
+                                    <h4 className="px-2 py-1 bg-blue-200 rounded-md text-sm">
+                                        Tailwind
+                                    </h4>
+                                </div>
+                                <p className="mb-4">
+                                    Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua.
+                                    Nam libero justo laoreet sit amet cursus
+                                    sit.
+                                </p>
+                                <div className="flex gap-4 mt-auto">
+                                    <a
+                                        href="https://github.com/chasacademy-robin-bergstrand/react-wordle-clone"
+                                        className="bg-blue-700 py-2 px-4 rounded-lg text-white ml-auto hover:bg-blue-800"
+                                    >
+                                        Github
+                                    </a>
+                                    <a
+                                        href="https://react-wordle-clone-xi.vercel.app/"
+                                        className="bg-blue-700 py-2 px-4 rounded-lg text-white hover:bg-blue-800"
+                                    >
+                                        View live
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-1/2 rounded-md overflow-hidden drop-shadow-2xl  flex flex-col">
+                            <Image
+                                className="h-96 object-cover object-left"
+                                src={"/quiz.png"}
+                                width="1000"
+                                height="1000"
+                            ></Image>
+                            <div className="p-6 border-2 flex-grow">
+                                <h3 className="text-2xl mb-2">QUIZ</h3>
+                                <div className="flex gap-2 mb-4">
+                                    <h4 className="px-2 py-1 bg-blue-200 rounded-md text-sm">
+                                        JavaScript
+                                    </h4>
+                                    <h4 className="px-2 py-1 bg-blue-200 rounded-md text-sm">
+                                        React
+                                    </h4>
+                                    <h4 className="px-2 py-1 bg-blue-200 rounded-md text-sm">
+                                        Redux
+                                    </h4>
+                                    <h4 className="px-2 py-1 bg-blue-200 rounded-md text-sm">
+                                        Tailwind
+                                    </h4>
+                                </div>
+                                <p className="mb-4">
+                                    Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua.
+                                    Nam libero justo laoreet sit amet cursus
+                                    sit.
+                                </p>
+                                <div className="flex gap-4">
+                                    <button className="bg-blue-700 py-2 px-4 rounded-lg text-white ml-auto">
+                                        Github
+                                    </button>
+                                    <button className="bg-blue-700 py-2 px-4 rounded-lg text-white">
+                                        View live
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <button className="bg-blue-700 py-2 px-4 rounded-lg text-white self-end mt-8">
+                        More projects
+                    </button>
+                </section>
+            </main>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    );
 }
